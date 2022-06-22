ef4cefe285992682befb1f81a004e7645f9432b7
MaxwellAnalytica <101760403+MaxwellAnalytica@users.noreply.github.com>
2022-06-22 23:24:40 +0800

    add include file

---
+++ file_search/src/include/datatype.h
@@ -0,0 +1,262 @@
+﻿/**********************************************************************
+*
+* Copyright (C)2015-2022 Maxwell Analytica. All rights reserved.
+*
+* @file datatype.h
+* @brief 
+* @details 
+* @author maxwell
+* @version 1.0.0
+* @date 2018-12-25 22:41:27.492
+*
+**********************************************************************/
+#ifndef DATA_TYPE_H
+#define DATA_TYPE_H
+
+#include <stdint.h>
+#include <string.h>
+
+#define WD_BLOCK_SIZE 4096
+
+/*
+*
+* @brief:	return value 
+* @details
+*/
+enum 
+{
+	WFC_Success		= 0,		/* success */
+	WFC_Error		= -1,		/* error */
+	WFC_Fatal		= -2,		/* fatal */
+	WFC_Abort		= -3,		/* abort */
+	WFC_Busy		= -4,		/* busy */
+};
+/*
+*
+* @brief:	filesystem type
+* @details: apply to corresponding scanner
+* 
+*/
+enum FileSystemCategory 
+{
+	FSC_None,					/* None */
+	FSC_Fat16,					/* FAT16 */
+	FSC_Fat32,					/* FAT32 */
+	FSC_ExFat,					/* exFAT */
+	FSC_Ntfs,					/* NTFS */
+	FSC_Hfsp,					/* MacOS Extends */
+	FSC_Apfs,					/* Apple filesystem */
+	FSC_Raw,					/* File carver */
+	FSC_RecycleBin,				/* Recycle bins */
+	FSC_Advance,				/* Advance file carver */
+};
+/*
+*
+* @brief:	data run list types
+* @details 
+*/
+enum 
+{
+	RLC_Unused		= 0,		/* no use */
+	RLC_General,				/* general data run list */
+	RLC_Inside,					/* resident file that data in a sector or cluster */
+};
+/*
+*
+* @brief:	scan types
+* @details
+*/
+enum 
+{
+	ST_Explore = 0,	/* explore files of volume */
+	ST_Delete,		/* quick scan, analyze the structure base on filesystem */
+	ST_Format,		/* format scan, sequential analyze sector reference filesystem */
+	ST_Raw,			/* general file carve */
+	ST_Copy			/* file copy */
+};
+/*
+*
+* @brief:	scan type flags
+* @details
+*/
+typedef enum _ScanTypeFlag
+{
+	STF_General		= 0x01,
+	STF_Location	= 0x0B,
+	STF_RecycleBin	= 0x0C,
+	STF_Image		= 0x0D, 
+}ScanTypeFlag;
+/*
+*
+* @brief:	Inject control
+* @details: direction: Out: core -> UI；In: core <- UI
+*/
+typedef enum _InjectControlOption
+{
+	IC_ActuatorOut			= 0x0000,
+	IC_ActuatorIn			= 0x0001,
+	IC_FileCarverOut		= 0x0002,
+	IC_FileCarverIn			= 0x0003,
+	IC_SettingOut			= 0x0005,
+	IC_SettingIn			= 0x0006,
+	IC_CheckCertificate		= 0x000A,
+	IC_EFSCertificate		= 0x000B,
+	IC_EFSpassword			= 0x000C,
+	IC_DiscardCertificate	= 0x000D,
+	IC_ProtectionStatus		= 0x000E,
+	IC_CreateCertificate	= 0x000F,
+	IC_EncryptedFlag		= 0x4000,
+}InjectControlOption;
+/*
+*
+* @brief:	Notice or Message types
+* @details 
+*/
+typedef enum _NotifyOption
+{
+	NO_Unknown = 0,
+	NO_Start,
+	NO_Stop,
+	NO_Pause,
+	NO_Resume,
+	NO_Milestone,
+	NO_Completed,
+	NO_FileInfo,
+	NO_Password,
+	NO_Progress,
+	NO_Disconnect,
+	NO_BadCluster,
+}NotifyOption;
+
+#pragma pack(1)
+/*
+*
+* @brief:	data run list, measured in sectors
+* @details
+*/
+struct Runlist {
+	uint64_t	Offset;		/* file offset at the runlist, measured in bytes*/
+	uint64_t	Start;		/* start sectors of runlist */
+	uint64_t	Number;		/* number sectors of runlist */
+	Runlist*	Next;		/* address of next runlist */
+	Runlist() {
+		Offset	= 0;
+		Start	= 0;
+		Number	= 0;
+		Next	= nullptr;
+	}
+};
+/*
+*
+* @brief:  data package
+* @details
+*/
+typedef struct _ClusterPackage
+{
+	int32_t			Option;					/* package type */
+	uint64_t		BlockNumber;			/* start sector of buffer */
+	char			Buffer[WD_BLOCK_SIZE];	/* data */
+	_ClusterPackage() {
+		Option		= 0;
+		BlockNumber = 0;
+		memset(Buffer, 0x00, WD_BLOCK_SIZE);
+	}
+}ClusterPackage, *PClusterPackage;
+/*
+*
+* @brief:	file information
+* @details 
+*/
+struct BaseInfo {
+	uint64_t	Id;					/* file unique id */
+	uint64_t	Pid;				/* parent id */
+	uint64_t	Did;				/* developer id */
+	uint64_t	Size;				/* file size，directory is 0 */
+	uint16_t	Flag;				/* delete flag，0 for not delete，1 for deleted */
+	Runlist*	Runlist;			/* data run list, file storage address on disk */
+	uint32_t	Category;			/* file type，0 for directory, 1 for file */
+	uint32_t	ScanType;			/* file from which scan type */
+	uint32_t	Attribute;			/* file attribute */
+	uint32_t	FileSystem;			/* file from which filesystem */
+	uint32_t	RunlistCategory;	/* 0 for no runlist; 1 general runlist; 2 for resident runlist*/
+	uint64_t	CreateTime;			/* file create datetime */
+	uint64_t	AccessTime;			/* file access datetime */
+	uint64_t	ModifyTime;			/* file modify datetime */
+	uint8_t		Name[256];			/* file name, limit 256 bytes */
+	BaseInfo() {
+		Id = 0;
+		Pid = 0;
+		Did = 0;
+		Size = 0;
+		Flag = 0;
+		Runlist = nullptr;
+		Category = 0;
+		ScanType = 0;
+		Attribute = 0;
+		FileSystem = 0;
+		RunlistCategory = 0;
+		CreateTime = 0;
+		AccessTime = 0;
+		ModifyTime = 0;
+		memset(Name, 0x00, 256);
+	}
+};
+/*
+*
+* @brief: FAT file information, inherit `BaseInfo`
+* @details 
+*/
+struct FatFileInfo : BaseInfo {
+	uint64_t	MaxClusterId;
+	uint64_t	RootSectorId;
+	uint32_t	SectorOfCluster;
+	uint32_t	SectorSize;
+};
+/*
+*
+* @brief: EXFAT file information, inherit `BaseInfo`
+* @details 
+*/
+struct ExFatFileInfo : BaseInfo {
+	uint64_t	MaxClusterId;
+	uint64_t	RootSectorId;
+	uint32_t	SectorOfCluster;
+	uint32_t	SectorSize;
+};
+/*
+*
+* @brief: NTFS file information, inherit `BaseInfo`
+* @details 
+*/
+struct NtfsFileInfo : BaseInfo {
+	uint64_t	MaxClusterId;
+	uint64_t	RootSectorId;
+	uint32_t	SectorOfCluster;
+	uint32_t	SectorSize;
+};
+/*
+*
+* @brief: HFSP file information, inherit `BaseInfo`
+* @details 
+*/
+struct HfspFileInfo : BaseInfo {
+	uint64_t	MaxClusterId;
+	uint64_t	RootSectorId;
+	uint32_t	SectorOfCluster;
+	uint32_t	SectorSize;
+};
+/*
+*
+* @brief: APFS file information, inherit `BaseInfo`
+* @details 
+*/
+struct ApfsFileInfo : BaseInfo {
+	uint64_t	MaxClusterId;
+	uint64_t	RootSectorId;
+	uint32_t	SectorOfCluster;
+	uint32_t	SectorSize;
+};
+
+#pragma pack()
+
+#endif /* DATA_TYPE_H */ 
---
+++ file_search/src/include/irecoverycore.h
@@ -0,0 +1,127 @@
+﻿/**********************************************************************
+*
+* Copyright (C)2015-2022 Weinbery Corporation. All rights reserved.
+*
+* @file irecoverycore.h
+* @brief 
+* @details 
+* @author wd
+* @version 1.0.0
+* @date 2021-10-09 22:41:27.492
+*
+**********************************************************************/
+#pragma once
+
+#include <stdint.h>
+#include <iostream>
+#include <string>
+#include "datatype.h"
+/*
+*
+* @brief 通知/消息代理（UI <=> 底层调度库）
+* @details 
+*/
+class INotifyDelegate
+{
+public:
+	/*
+	*
+	* @brief 通知回调函数，type由NotifyOption定义，ptr可自定义（结构体，字符串等）
+	* @details 
+	*/
+	virtual int32_t OnNotify(int32_t type, void* ptr = nullptr, int64_t *size = nullptr) = 0;
+};
+/*
+*
+* @brief 数据恢复核心抽象类，底层主调度库继承之
+* @details 提供各数据恢复底层需要的API
+*/
+class IRecoveryCore
+{
+public:
+	/*
+	*
+	* @brief 设置消息内转代理ITransferDelegate
+	* @details 
+	*/
+	virtual int32_t SetDelegate(INotifyDelegate* delegate) = 0;
+	/*
+	*
+	* @brief 初始化
+	* @details 
+	*/
+	virtual int32_t Init() = 0;
+	/*
+	*
+	* @brief 设备枚举
+	* @details 
+	*/
+	virtual int32_t Explore(char* info) = 0;
+	/*
+	*
+	* @brief 开始扫描，UI只调用该函数一次，底层库自行实现快速扫描+格式化扫/文件雕刻
+	* @details 
+	*/
+	virtual int32_t Scan(const char* params, int32_t size) = 0;
+	/*
+	*
+	* @brief 停止扫描
+	* @details 
+	*/
+	virtual int32_t Stop(int32_t option = 0) = 0;
+	/*
+	*
+	* @brief 暂停扫描
+	* @details 
+	*/
+	virtual int32_t Pause(int32_t option = 0) = 0;
+	/*
+	*
+	* @brief 继续扫描
+	* @details 
+	*/
+	virtual int32_t Resume(int32_t option = 0) = 0;
+	/*
+	*
+	* @brief 文件恢复（不是必须使用）
+	* @details 
+	*/
+	virtual int32_t Recvoer(char* buffer, const char* params, int32_t size) = 0;
+	/*
+	*
+	* @brief 文件预览（不是必须使用）
+	* @details 
+	*/
+	virtual int32_t Preview(char* buffer, const char* params, int32_t size) = 0;
+	/*
+	*
+	* @brief 控制注入，例如可注入参数，随机设置扫描的磁盘扇区偏移，修改扫描进度
+	* @details 
+	*/
+	virtual int32_t InjectControl(char* params, int32_t* size, int32_t option = 0) = 0;
+	/*
+	*
+	* @brief 通过底层读取纯数据
+	* @details 
+	*/
+	virtual int32_t ReadRawData(char* buffer, int64_t offset, int32_t size, void* attach = nullptr) = 0;
+	/*
+	*
+	* @brief 调用底层进行文件恢复，用于APFS这样的只能通过底层进行文件恢复的文件系统
+	* @details 
+	*/
+	virtual int32_t GeneralRecover(char* filePath, int32_t index, int64_t id, int64_t size, int32_t option = 0) = 0;
+	/*
+	*
+	* @brief 调用底层读取文件数据，用于APFS这样的只能通过底层读取文件数据的文件系统
+	* @details 
+	*/
+	virtual int32_t GeneralReadRawData(char* buffer, int32_t index, int64_t id, int64_t offset, int64_t size, int32_t option = 0) = 0;
+
+};
+/*
+*
+* @brief 底层库创建的API
+* @details 
+*/
+extern "C" int32_t __stdcall CreateRecoveryCore(IRecoveryCore** core);
